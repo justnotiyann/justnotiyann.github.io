1b0d4be5df8b6e585cf06305e38cd9a4feedd11f
just_iyan <muhammadfitrian0712@gmail.com>
2023-05-28 03:30:16 +0700

    add new website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+
+  theme: {
+    container: {
+      center: true,
+      padding: '0.5em'
+    },
+    extend: {
+      colors: {
+        primary: {
+          "teal": "#0d9488",
+          "gray": "#4b5563"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
